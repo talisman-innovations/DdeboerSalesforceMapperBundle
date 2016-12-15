dfe538023cf7e7e3dcb4412842732700226985c8
Gnilya <paul.ayling@me.com>
2016-12-15 12:31:59 +0000

    Add converter fix

--- Request/ParamConverter/SalesforceParamConverter.php
+++ Request/ParamConverter/SalesforceParamConverter.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace Ddeboer\Salesforce\MapperBundle\Request\ParamConverter;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface;
 use Ddeboer\Salesforce\MapperBundle\Mapper;
-
 /**
  * This param converter convers a Salesforce id into a Salesforce object, using
  * the Salesforce mapper for lookup.
@@ -20,14 +17,12 @@ class SalesforceParamConverter implements ParamConverterInterface
      * @var Mapper
      */
     protected $mapper;
-
     /**
      * @var array
      *
      * Property name => Salesforce model name
      */
     protected $mappings;
-
     /**
      * {@inheritdoc}
      */
@@ -36,16 +31,14 @@ class SalesforceParamConverter implements ParamConverterInterface
         $this->mapper = $mapper;
         $this->mappings = $mappings;
     }
-
     /**
      * {@inheritdoc}
      */
-    public function apply(Request $request, ConfigurationInterface $configuration)
+    public function apply(Request $request, ParamConverter $configuration)
     {
         // @todo Is it smart to do this based on variable name? Perhaps it's
         // better to, here also, look at class name?
         $class = $configuration->getClass();
-
         // If request has property set that corresponds to the Salesforce model,
         // go ahead and do the param conversion.
         $propertyName = \array_search($class, $this->mappings);
@@ -55,17 +48,14 @@ class SalesforceParamConverter implements ParamConverterInterface
             if (!$model) {
                 throw new NotFoundHttpException('Model with id ' . $id . ' not found');
             }
-
             $request->attributes->set($configuration->getName(), $model);
         }
     }
-
     /**
      * {@inheritdoc}
      */
-    public function supports(ConfigurationInterface $configuration)
+    public function supports(ParamConverter $configuration)
     {
         return in_array($configuration->getClass(), $this->mappings);
     }
 }
-
