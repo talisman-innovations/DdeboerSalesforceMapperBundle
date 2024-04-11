ba52c352dde1d0040d0bd719417e26f184349f9c
Steven Brookes <steven.brookes@talisman-innovations.com>
2024-04-11 16:28:26 +0100

    Update dependencies and types

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,7 +15,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritDoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('ddeboer_salesforce_mapper');
         $treeBuilder
